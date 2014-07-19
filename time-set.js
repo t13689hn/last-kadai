98bcd8f4b0c2556fee9fbe420f29bd8c92cb1315
t13909rm <t13909rm@sfc.keio.ac.jp>
2014-07-19 15:04:09 +0900

    スペルミスの修正　JS

--- time-set.js
+++ time-set.js
@@ -25,26 +25,26 @@ Hello2.addEventListener("click",Hello)
   //var time = document.getElementById("jikan")
   //console.log(document.write);
 
-var shafful = new Array(
+var shahhul = new Array(
   {name:src="kyoku/m1.mp3",time:245},
   {name:src="kyoku/m2.mp3",time:320},
   {name:src="kyoku/m3.mp3",time:229}  ,
-  {name:src="kyoku/m4.mp3",time:172:},
-  {name:src="kyoku/m5.mp3",time:271:},
+  {name:src="kyoku/m4.mp3",time:172},
+  {name:src="kyoku/m5.mp3",time:271},
   {name:src="kyoku/m6.mp3",time:172},
   {name:src="kyoku/m7.mp3",time:255},
   {name:src="kyoku/m8.mp3",time:260},
-  {name:src="kyoku/m10.mp3",time:249},
-  {name:src="kyoku/m11.mp3",time:240},
-  {name:src="kyoku/m12.mp3",time:242},
-  {name:src="kyoku/m13.mp3",time:218},
-  {name:src="kyoku/m14.mp3",time:240},
-  {name:src="kyoku/m15.mp3",time:150},
-  {name:src="kyoku/m16.mp3",time:199},
-  {name:src="kyoku/m17.mp3",time:191},
-  {name:src="kyoku/m18.mp3",time:288},
-  {name:src="kyoku/m19.mp3",time:197},
-  {name:src="kyoku/m20.mp3",time:172}
+  {name:src="kyoku/m9.mp3",time:249},
+  {name:src="kyoku/m10.mp3",time:240},
+  {name:src="kyoku/m11.mp3",time:242},
+  {name:src="kyoku/m12.mp3",time:218},
+  {name:src="kyoku/m13.mp3",time:240},
+  {name:src="kyoku/m14.mp3",time:150},
+  {name:src="kyoku/m15.mp3",time:199},
+  {name:src="kyoku/m16.mp3",time:191},
+  {name:src="kyoku/m17.mp3",time:288},
+  {name:src="kyoku/m18.mp3",time:197},
+  {name:src="kyoku/m19.mp3",time:172}
 
   );
 
@@ -58,14 +58,14 @@ for ( i = 0 ; i < 100 ; i++ )
   var str2 = shahhul[rnd];  //配列colの乱数で決定した要素
 
   //配列の各要素を入れ替える
-  col[rnd] = str1;
-  col[0]   = str2;
+  shahhul[rnd] = str1;
+  shahhul[0]   = str2;
 }
 
-consol.log(shahhul[2]);
+console.log(shahhul[2]);
 
 var playlisttime = 0;
-var playarray = new array{};
+var playarray = [];
 function  createplaylist(){
 	while(jikan>playlisttime){
 			var n=0;
